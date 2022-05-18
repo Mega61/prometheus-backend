d9b1c00c51e2f52a75a0d99156d733b4849cd7c8
Juan E Daza <megauploads61@gmail.com>
2022-05-17 21:43:54 -0500

    Cross Origin

--- src/main/java/com/prometheus/prometheusbackend/controller/UserController.java
+++ src/main/java/com/prometheus/prometheusbackend/controller/UserController.java
@@ -7,6 +7,7 @@ import com.prometheus.prometheusbackend.entity.User;
 import com.prometheus.prometheusbackend.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,26 +24,31 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/users")
     public String createUser(@RequestBody User user) throws InterruptedException, ExecutionException{
         return userService.createUser(user);
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/users/{client_id}")
     public User getUser(@PathVariable String client_id) throws InterruptedException, ExecutionException{
         return userService.getUserDetails(client_id);
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/users")
     public List<User> getAllUsers() throws InterruptedException, ExecutionException{
         return userService.getAllUserDetails();
     }
 
+    @CrossOrigin(origins = "*")
     @PutMapping("/users")
     public String updateUser(@RequestBody User user) throws InterruptedException, ExecutionException{
         return userService.updateUser(user);
     }
     
+    @CrossOrigin(origins = "*")
     @DeleteMapping("/users/{client_id}")
     public String deleteUser(@PathVariable String client_id) throws ExecutionException, InterruptedException{
         return userService.deleteUser(client_id);
