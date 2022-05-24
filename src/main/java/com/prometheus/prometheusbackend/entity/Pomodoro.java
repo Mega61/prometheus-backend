9e854238465c2df70a93e341f5ef5007483a619d
Juan E Daza <megauploads61@gmail.com>
2022-05-23 21:56:52 -0500

    Custom pomodoro constructor

--- src/main/java/com/prometheus/prometheusbackend/entity/Pomodoro.java
+++ src/main/java/com/prometheus/prometheusbackend/entity/Pomodoro.java
@@ -5,8 +5,20 @@ public class Pomodoro {
     private String pomodoro_id;
     private int work_time;
     private int short_rest;
-    private int long_rest;
     private int pomodori_quantity;
+    private int pauses_quantity;
+    private int actual_work_time;
+
+
+    public Pomodoro(String pomodoro_id, int work_time, int short_rest, int pomodori_quantity, int pauses_quantity) {
+        this.pomodoro_id = pomodoro_id;
+        this.work_time = work_time;
+        this.short_rest = short_rest;
+        this.pomodori_quantity = pomodori_quantity;
+        this.pauses_quantity = pauses_quantity;
+        setActual_work_time();
+    }
+
 
     public String getPomodoro_id() {
         return this.pomodoro_id;
@@ -32,14 +44,6 @@ public class Pomodoro {
         this.short_rest = short_rest;
     }
 
-    public int getLong_rest() {
-        return this.long_rest;
-    }
-
-    public void setLong_rest(int long_rest) {
-        this.long_rest = long_rest;
-    }
-
     public int getPomodori_quantity() {
         return this.pomodori_quantity;
     }
@@ -47,4 +51,22 @@ public class Pomodoro {
     public void setPomodori_quantity(int pomodori_quantity) {
         this.pomodori_quantity = pomodori_quantity;
     }
+
+
+    public int getPauses_quantity() {
+        return this.pauses_quantity;
+    }
+
+    public void setPauses_quantity(int pauses_quantity) {
+        this.pauses_quantity = pauses_quantity;
+    }
+
+    public int getActual_work_time() {
+        return this.actual_work_time;
+    }
+
+    public void setActual_work_time() {
+        this.actual_work_time = this.work_time * this.pomodori_quantity;
+    }
+
 }
